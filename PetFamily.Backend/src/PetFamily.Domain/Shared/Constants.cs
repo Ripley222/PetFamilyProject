a5b2c2e87f9ff08004cb5b896a57aa90638a2dc0
Nikita Petrunyak <ox1om2002@gmail.com>
2025-06-11 18:57:32 +0300

    Deleted extra spaces

--- PetFamily.Backend/src/PetFamily.Domain/Shared/Constants.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Shared/Constants.cs
@@ -3,14 +3,8 @@
 public class Constants
 {
     public const int MAX_LENGTH_TITLE = 100;
-    
     public const int MIN_LENGTH_DESCRIPTION = 5;
-    
     public const int MAX_LENGTH_DESCRIPTION = 1000;
-    
     public const int MIN_LENGTH_NAME = 2;
-
     public const int MAX_LENGTH_NAME = 100;
-    
-    
 }
