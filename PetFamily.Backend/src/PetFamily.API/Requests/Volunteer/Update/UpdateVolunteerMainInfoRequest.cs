25c2875278e1c3d8144f6746179d551af1239884
Ripley222 <nik.petrunyak@bk.ru>
2025-08-25 17:34:57 +0300

    добавил ручной маппинг requests в commands

--- PetFamily.Backend/src/PetFamily.API/Requests/Volunteer/Update/UpdateVolunteerMainInfoRequest.cs
+++ PetFamily.Backend/src/PetFamily.API/Requests/Volunteer/Update/UpdateVolunteerMainInfoRequest.cs
@@ -1,10 +1,23 @@
-﻿namespace PetFamily.API.Requests.Volunteer.Update;
+﻿using PetFamily.Application.VolunteersFeatures.Update.MainInfo;
+
+namespace PetFamily.API.Requests.Volunteer.Update;
 
 public record UpdateVolunteerMainInfoRequest(
-    string FirstName, 
+    string FirstName,
     string MiddleName,
-    string LastName, 
-    string EmailAddress, 
-    string Description, 
-    int YearsOfExperience, 
-    string PhoneNumber);
+    string LastName,
+    string EmailAddress,
+    string Description,
+    int YearsOfExperience,
+    string PhoneNumber)
+{
+    public UpdateMainInfoCommand ToCommand(Guid volunteerId) =>
+        new(volunteerId,
+            FirstName,
+            MiddleName,
+            LastName,
+            EmailAddress,
+            Description,
+            YearsOfExperience,
+            PhoneNumber);
+}
--- PetFamily.Backend/src/PetFamily.API/Requests/Volunteer/Update/UpdateVolunteerRequisitesRequest.cs
+++ PetFamily.Backend/src/PetFamily.API/Requests/Volunteer/Update/UpdateVolunteerRequisitesRequest.cs
@@ -1,6 +1,12 @@
 ﻿using PetFamily.Application.VolunteersFeatures.DTOs;
+using PetFamily.Application.VolunteersFeatures.Update.Requisites;
 
 namespace PetFamily.API.Requests.Volunteer.Update;
 
 public record UpdateVolunteerRequisitesRequest(
-    IEnumerable<RequisitesDto> Requisites);
+    IEnumerable<RequisitesDto> Requisites)
+{
+    public UpdateRequisitesCommand ToCommand(Guid volunteerId) =>
+        new(volunteerId,
+            Requisites);
+}
