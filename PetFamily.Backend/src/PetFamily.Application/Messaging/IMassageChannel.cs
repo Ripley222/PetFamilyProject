52a6b3e0d283f1d8827897d89a5f7d81f6b39b60
Ripley222 <nik.petrunyak@bk.ru>
2025-08-25 13:16:23 +0300

    добавлен backgorund сервис, который очищает minio от лишних файлов

---
+++ PetFamily.Backend/src/PetFamily.Application/Messaging/IMassageChannel.cs
@@ -0,0 +1,10 @@
+﻿using PetFamily.Application.FileProvider;
+
+namespace PetFamily.Application.Messaging;
+
+public interface IMassageChannel<TMessage>
+{
+    Task WriteAsync(TMessage paths, CancellationToken cancellationToken = default);
+
+    Task<TMessage> ReadAsync(CancellationToken cancellationToken = default);
+}
