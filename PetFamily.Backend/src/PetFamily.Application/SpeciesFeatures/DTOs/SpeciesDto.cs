2f243b1f44721b1b75c04256eaa0fd319f381424
Ripley222 <nik.petrunyak@bk.ru>
2025-09-26 12:17:56 +0300

    реализация получения всех species животного

---
+++ PetFamily.Backend/src/PetFamily.Application/SpeciesFeatures/DTOs/SpeciesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PetFamily.Application.SpeciesFeatures.DTOs;
+
+public record GetSpeciesDto(
+    IEnumerable<SpeciesDto> Species);
+
+public record SpeciesDto(
+    Guid SpeciesId,
+    string Name);
